f33b11b0c9cd2a63c9ad33be71ee8dc2de7dfdea
cromx123 <73903821+cromx123@users.noreply.github.com>
2023-12-12 20:06:40 -0300

    atdsfasdf

--- backend.php
+++ backend.php
@@ -19,21 +19,28 @@
         $email = htmlspecialchars($_POST["email"]);
         $contrasena = htmlspecialchars($_POST["contrasena"]);
         $fecha_nacimiento = htmlspecialchars($_POST["fecha_nacimiento"]);
-
+        $tipo= 1;
         // Validar los datos (agrega más validaciones según sea necesario)
 
         // Consulta SQL preparada para evitar inyección de SQL
-        $sql = "INSERT INTO usuarios (US_nombre, US_mail, US_pass) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO usuario (US_nombre, US_mail, US_pass, TIPOUS_ID) VALUES (?, ?, ?, ?);";
 
         // Preparar la declaración
         $stmt = $conn->prepare($sql);
 
         // Vincular parámetros
-        $stmt->bind_param("sss", $nombre, $email, $contrasena);
+        $stmt->bind_param("ssss", $nombre, $email, $contrasena, $tipo);
 
+        // Ejecutar la declaración
+        if ($stmt->execute()) {
+            header("Location: ingreso.html");
+        } else {
+            echo "Error en el registro: " . $stmt->error;
+        }
         // Cerrar la declaración y la conexión
         $stmt->close();
     }
+    
     // Cerrar la conexión
     $conn->close();
 ?>
