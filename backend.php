692a40bf0e162259a3f25a02de845eceb7737bf1
cromx123 <73903821+cromx123@users.noreply.github.com>
2023-12-12 20:43:19 -0300

    mejora 1.1

--- backend.php
+++ backend.php
@@ -14,33 +14,61 @@
     }
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Recuperar datos del formulario
-        $nombre = htmlspecialchars($_POST["nombre"]);
-        $email = htmlspecialchars($_POST["email"]);
-        $contrasena = htmlspecialchars($_POST["contrasena"]);
-        $fecha_nacimiento = htmlspecialchars($_POST["fecha_nacimiento"]);
-        $tipo= 1;
-        // Validar los datos (agrega más validaciones según sea necesario)
+        if (isset($_POST["registro"])) {
+            // Recuperar datos del formulario
+            $nombre = htmlspecialchars($_POST["nombre"]);
+            $email = htmlspecialchars($_POST["email"]);
+            $contrasena = htmlspecialchars($_POST["contrasena"]);
+            $fecha_nacimiento = htmlspecialchars($_POST["fecha_nacimiento"]);
+            $tipo= 1;
+            // Validar los datos (agrega más validaciones según sea necesario)
 
-        // Consulta SQL preparada para evitar inyección de SQL
-        $sql = "INSERT INTO usuario (US_nombre, US_mail, US_pass, TIPOUS_ID) VALUES (?, ?, ?, ?);";
+            // Consulta SQL preparada para evitar inyección de SQL
+            $sql = "INSERT INTO usuario (US_nombre, US_mail, US_pass, TIPOUS_ID) VALUES (?, ?, ?, ?);";
 
-        // Preparar la declaración
-        $stmt = $conn->prepare($sql);
+            // Preparar la declaración
+            $stmt = $conn->prepare($sql);
 
-        // Vincular parámetros
-        $stmt->bind_param("ssss", $nombre, $email, $contrasena, $tipo);
+            // Vincular parámetros
+            $stmt->bind_param("ssss", $nombre, $email, $contrasena, $tipo);
 
-        // Ejecutar la declaración
-        if ($stmt->execute()) {
-            header("Location: ingreso.html");
-        } else {
-            echo "Error en el registro: " . $stmt->error;
+            // Ejecutar la declaración
+            if ($stmt->execute()) {
+                header("Location: ingreso.html");
+            } else {
+                echo "Error en el registro: " . $stmt->error;
+            }
+            // Cerrar la declaración y la conexión
+            $stmt->close();
         }
-        // Cerrar la declaración y la conexión
-        $stmt->close();
+        elseif (isset($_POST["login"])) {
+            $email = htmlspecialchars($_POST["email"]);
+            $contrasena = htmlspecialchars($_POST["contrasena"]);
+            // Realizar consulta SQL
+            $sql = "SELECT US_id, US_pass, US_mail FROM usuario WHERE US_mail = ?";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            // Verificar si el usuario existe y las credenciales son correctas
+            if ($row = $result->fetch_assoc()) {
+                if (password_verify($contrasena, $row['US_pass'])) {
+                    // Credenciales válidas, iniciar sesión
+                    $_SESSION["usuario_id"] = $row['US_id'];
+
+                    header("Location: index.html");
+                    exit();
+                } else {
+                    echo "Contraseña incorrecta. Usted proporcionó: $contrasena";
+                }
+            } else {
+                echo "Usuario no encontrado.";
+            }
+            $stmt->close();
+        }
     }
-    
     // Cerrar la conexión
     $conn->close();
 ?>
