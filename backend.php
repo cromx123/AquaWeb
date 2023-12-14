f39bf4e838ec65e71796a89033ca96a628b8eb18
cromx123 <73903821+cromx123@users.noreply.github.com>
2023-12-14 01:45:51 -0300

    Se dejo listo el Catálogo del software
    ahora el catálogo se puede ordenar o buscar por un filtro, se puede mejorar combinando las 2 cosas

--- backend.php
+++ backend.php
@@ -1,116 +1,145 @@
 <?php
-    // Configuración de la base de datos
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "myweb";
-    
-    // Crear conexión
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Verificar la conexión
-    if ($conn->connect_error) {
-        die("La conexión a la base de datos falló: " . $conn->connect_error);
-    }
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["registro"])) {
-            // Recuperar datos del formulario
-            $nombre = htmlspecialchars($_POST["nombre"]);
-            $email = htmlspecialchars($_POST["email"]);
-            $contrasena = htmlspecialchars($_POST["contrasena"]);
-            $fecha_nacimiento = htmlspecialchars($_POST["fecha_nacimiento"]);
-            $tipo= 1;
-            // Validar los datos (agrega más validaciones según sea necesario)
+// Configuración de la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myweb";
 
-            // Consulta SQL preparada para evitar inyección de SQL
-            $sql = "INSERT INTO usuario (US_nombre, US_mail, US_pass, TIPOUS_ID) VALUES (?, ?, ?, ?);";
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("La conexión a la base de datos falló: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["registro"])) {
+        // Recuperar datos del formulario
+        $nombre = htmlspecialchars($_POST["nombre"]);
+        $apellido = htmlspecialchars($_POST["apellido"]);
+        $telefono = htmlspecialchars($_POST["telefono"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $contrasena = htmlspecialchars($_POST["contrasena"]);
+        $tipo = 1;
+        $calle = htmlspecialchars($_POST["calle"]);
+        $callenum = htmlspecialchars($_POST["direccionnum"]);
+        $comuna = htmlspecialchars($_POST["comuna"]);
+        $US_id = rand(1000, 999999);
+        // Iniciar una transacción
+        $conn->begin_transaction();
+
+        try {
+            // Consulta SQL preparada para evitar inyección de SQL - Tabla 2 (direccion)
+            $sqlDIR = "INSERT INTO direccion(DIR_CALLE,DIR_NUM,DIR_COMUNA) VALUES (?, ?, ?);";
 
             // Preparar la declaración
-            $stmt = $conn->prepare($sql);
+            $stmtDIR = $conn->prepare($sqlDIR);
+
+            // Vincular parámetros (ajusta según tus campos)
+            $stmtDIR->bind_param("sss", $calle, $callenum, $comuna);
+
+            // Ejecutar la declaración para la tabla 2
+            $stmtDIR->execute();
+
+            // Obtener el DIR_id recién insertado
+            $DIR_id = $stmtDIR->insert_id;
+
+            // Cerrar la declaración para la tabla 2
+            $stmtDIR->close();
+
+            // Consulta SQL preparada para evitar inyección de SQL - Tabla 1 (usuario)
+            $sqlUsuario = "INSERT INTO usuario (US_id, US_nombre, US_apellido, US_fono, US_mail, US_pass, TIPOUS_ID, DIR_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+
+            // Preparar la declaración
+            $stmtUsuario = $conn->prepare($sqlUsuario);
 
             // Vincular parámetros
-            $stmt->bind_param("ssss", $nombre, $email, $contrasena, $tipo);
+            $stmtUsuario->bind_param("issssssi", $US_id, $nombre, $apellido, $telefono, $email, $contrasena, $tipo, $DIR_id);
 
-            // Ejecutar la declaración
-            if ($stmt->execute()) {
-                header("Location: ingreso.html");
-            } else {
-                echo "Error en el registro: " . $stmt->error;
-            }
-            // Cerrar la declaración y la conexión
-            $stmt->close();
+            // Ejecutar la declaración para la tabla 1
+            $stmtUsuario->execute();
+            $stmtUsuario->close();
+
+            // Confirmar la transacción
+            $conn->commit();
+
+            // Redireccionar a la página de ingreso
+            header("Location: ingreso.html");
+            exit();
+        } catch (Exception $e) {
+            // Revertir la transacción en caso de error
+            $conn->rollback();
+            echo "Error en el registro: " . $e->getMessage();
         }
-        elseif (isset($_POST["login"])) {
-            $email = htmlspecialchars($_POST["email"]);
-            $contrasena = htmlspecialchars($_POST["contrasena"]);
-            // Realizar consulta SQL
-            $sql = "SELECT US_id, US_pass, US_mail, TIPOUS_ID FROM usuario WHERE US_mail = ?";
+    } elseif (isset($_POST["login"])) {
+        $email = htmlspecialchars($_POST["email"]);
+        $contrasena = htmlspecialchars($_POST["contrasena"]);
+        // Realizar consulta SQL
+        $sql = "SELECT US_id, US_pass, US_mail, TIPOUS_ID FROM usuario WHERE US_mail = ?";
 
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $result = $stmt->get_result();
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-            // Verificar si el usuario existe y las credenciales son correctas
-            if ($row = $result->fetch_assoc()) {
-                if ($contrasena == $row['US_pass']) {
-                    // Credenciales válidas, iniciar sesión
-                    $_SESSION["usuario"] = $row['US_nombre'];
-                    if($row['TIPOUS_ID'] == 0){
-                        header("Location: balancesadm.html");
-                    }else if($row['TIPOUS_ID'] == 1){
-                        header("Location: balancesusu.html");
-                    }else if($row['TIPOUS_ID'] == 2){
-                        header("Location: actualizarestado.html");
-                    }
-                    
-                    exit();
-                } else {
-                    echo "Contraseña incorrecta." ;
+        // Verificar si el usuario existe y las credenciales son correctas
+        if ($row = $result->fetch_assoc()) {
+            if ($contrasena == $row['US_pass']) {
+                // Credenciales válidas, iniciar sesión
+                $_SESSION["usuario"] = $row['US_nombre'];
+                if($row['TIPOUS_ID'] == 0){
+                    header("Location: balancesadm.html");
+                }else if($row['TIPOUS_ID'] == 1){
+                    header("Location: balancesusu.html");
+                }else if($row['TIPOUS_ID'] == 2){
+                    header("Location: actualizarestado.html");
                 }
+                
+                exit();
             } else {
-                echo "Usuario no encontrado.";
+                echo "Contraseña incorrecta." ;
             }
-            $stmt->close();
+        } else {
+            echo "Usuario no encontrado.";
         }
-        elseif (isset($_POST["buscar"])){
-            $searchTerm = htmlspecialchars($_POST["searchTerm"]);
-            // Realizar la búsqueda en la base de datos
-            $sql = "SELECT * FROM bidon WHERE BID_nom LIKE ? OR BID_precio LIKE ? OR BID_litros LIKE ?";
-
-            $stmt = $conn->prepare($sql);
-            
-            // Concatenar '%' al término de búsqueda para buscar coincidencias parciales
-            $searchTerm = "%" . $searchTerm . "%";
-
-            $stmt->bind_param("sss", $searchTerm, $searchTerm, $searchTerm);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            // Mostrar resultados de la búsqueda
-            
-            if ($result->num_rows > 0) {
-                echo "<div id='results-container'>";
-                while ($row = $result->fetch_assoc()) {
-                    echo "<p>";
-                    echo "<p>Nombre: " . $row['BID_nom'] . "<br>";
-                    echo "Precio: " . $row['BID_precio'] . "<br>";
-                    echo "Litros: " . $row['BID_litros'] . "<br>";
-                    echo "Stock: " . $row['BID_stock'] . "</p>";
-                    // Mostrar la imagen
-                    echo "<img src='images/" . $row['BID_imagen_url'] . "' alt='Imagen del bidón'>";
-                    echo "</p>";
-                }
-                echo "</div>";
-            } else {
-                echo "<p>No se encontraron resultados.</p>";
-            }
-            $stmt->close();
-        }
+        $stmt->close();
     }
-    // Cerrar la conexión
-    $conn->close();
+    elseif (isset($_POST["buscar"])){
+        $searchTerm = htmlspecialchars($_POST["searchTerm"]);
+        // Realizar la búsqueda en la base de datos
+        $sql = "SELECT * FROM bidon WHERE BID_nom LIKE ? OR BID_precio LIKE ? OR BID_litros LIKE ?";
+
+        $stmt = $conn->prepare($sql);
+        
+        // Concatenar '%' al término de búsqueda para buscar coincidencias parciales
+        $searchTerm = "%" . $searchTerm . "%";
+
+        $stmt->bind_param("sss", $searchTerm, $searchTerm, $searchTerm);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Mostrar resultados de la búsqueda
+        
+        if ($result->num_rows > 0) {
+            echo "<div id='results-container'>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<p>";
+                echo "<p>Nombre: " . $row['BID_nom'] . "<br>";
+                echo "Precio: " . $row['BID_precio'] . "<br>";
+                echo "Litros: " . $row['BID_litros'] . "<br>";
+                echo "Stock: " . $row['BID_stock'] . "</p>";
+                // Mostrar la imagen
+                echo "<img src='images/" . $row['BID_imagen_url'] . "' alt='Imagen del bidón'>";
+                echo "</p>";
+            }
+            echo "</div>";
+        } else {
+            echo "<p>No se encontraron resultados.</p>";
+        }
+        $stmt->close();
+    }
+}
+// Cerrar la conexión
+$conn->close();
 ?>
-
-
--- catalogo.php
+++ catalogo.php
@@ -22,65 +22,125 @@
             </nav>
         </div>
     </header>
-    <h1>Catálogo de Productos</h1>
-
-    <div class="search-container">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <input type="text" name="searchTerm" placeholder="Buscar...">
-            <button type="submit" name="buscar">Buscar</button>
-        </form>
-    </div>
-
-    <div id="results-container">
-        <?php
-            // Configuración de la base de datos
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "myweb";
-            
-            // Crear conexión
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            
-            // Verificar la conexión
-            if ($conn->connect_error) {
-                die("La conexión a la base de datos falló: " . $conn->connect_error);
-            }
-            $searchTerm = isset($_POST["searchTerm"]) ? htmlspecialchars($_POST["searchTerm"]) : "";
-            // Realizar la búsqueda en la base de datos
-            $sql = "SELECT * FROM bidon WHERE BID_nom LIKE ? OR BID_precio LIKE ? OR BID_litros LIKE ?";
-
-            $stmt = $conn->prepare($sql);
-            
-            // Concatenar '%' al término de búsqueda para buscar coincidencias parciales
-            $searchTerm = "%" . $searchTerm . "%";
-
-            $stmt->bind_param("sss", $searchTerm, $searchTerm, $searchTerm);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            // Mostrar resultados de la búsqueda
-            
-            if ($result->num_rows > 0) {
-                echo "<div id='results-container'>";
-                while ($row = $result->fetch_assoc()) {
-                    echo "<p>";
-                    echo "<p>Nombre: " . $row['BID_nom'] . "<br>";
-                    echo "Precio: " . $row['BID_precio'] . "<br>";
-                    echo "Litros: " . $row['BID_litros'] . "<br>";
-                    echo "Stock: " . $row['BID_stock'] . "</p>";
-                    // Mostrar la imagen
-                    echo "<img src='images/" . $row['BID_imagen_url'] . "' alt='Imagen del bidón'>";
-                    echo "</p>";
-                }
-                echo "</div>";
-            } else {
-                echo "<p>No se encontraron resultados.</p>";
-            }
-            $conn->close();
-        ?>
+    <div class="titulo">
+        <h1>Catálogo de Productos</h1>
     </div>
     
+    <div class="todo">
+        <div class="filtros">
+            <h2>Filtrado de productos</h2>
+            <div class="buscador_de_contenedores">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <input type="text" name="searchTerm" placeholder="Buscar...">
+                    <button type="submit" name="buscar">Buscar</button>
+                </form>
+            </div>
+            <div class="filtros-eleccion">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <div class="custom-dropdown">
+                        <ul class="options-list">
+                            <!-- Agrega value="mas_vendido" a cada botón -->
+                            <li><button type="submit" name="orden" value="mas_vendido">Más vendido</button></li>
+                            <li><button type="submit" name="orden" value="mejor_valorado">Mejor Valorado</button></li>
+                            <li><button type="submit" name="orden" value="precio_ascendente">Precio Ascendente</button></li>
+                            <li><button type="submit" name="orden" value="precio_descendente">Precio Descendente</button></li>
+                            <li><button type="submit" name="orden" value="nombre_ascendente">Nombre Ascendente</button></li>
+                            <li><button type="submit" name="orden" value="nombre_descendente">Nombre Descendente</button></li>
+                        </ul>
+                    </div>
+                </form>
+            </div>
+        </div>
+        
+        <div class="results-container">
+            <?php
+                // Configuración de la base de datos
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "myweb";
+                
+                // Crear conexión
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                
+                // Verificar la conexión
+                if ($conn->connect_error) {
+                    die("La conexión a la base de datos falló: " . $conn->connect_error);
+                }
+                $searchTerm = isset($_POST["searchTerm"]) ? htmlspecialchars($_POST["searchTerm"]) : "";
+                // Realizar la búsqueda en la base de datos
+                $sql = "SELECT * FROM bidon WHERE BID_nom LIKE ? OR BID_precio = ? OR BID_litros = ?";
+
+                
+                // Verifica si se ha seleccionado una opción de orden
+                if (isset($_POST["orden"])) {
+                    $opcionOrden = $_POST["orden"];
+                
+                    // Aplica la lógica de ordenación según la opción seleccionada
+                    switch ($opcionOrden) {
+                        case "mas_vendido":
+                            // Lógica de ordenación para "Más vendido"
+                            $orderBy = " ORDER BY BID_cantVAL DESC"; // Reemplaza "ventas_columna" con el nombre real de la columna de ventas en tu base de datos
+                            break;
+                        case "mejor_valorado":
+                            // Lógica de ordenación para "Mejor Valorado"
+                            $orderBy = " ORDER BY BID_val DESC"; // Reemplaza "valoracion_columna" con el nombre real de la columna de valoración en tu base de datos
+                            break;
+                        case "precio_ascendente":
+                            // Lógica de ordenación para "Precio Ascendente"
+                            $orderBy = " ORDER BY BID_precio ASC";
+                            break;
+                        case "precio_descendente":
+                            // Lógica de ordenación para "Precio Descendente"
+                            $orderBy = " ORDER BY BID_precio DESC";
+                            break;
+                        case "nombre_ascendente":
+                            // Lógica de ordenación para "Nombre Ascendente"
+                            $orderBy = " ORDER BY BID_nom ASC";
+                            break;
+                        case "nombre_descendente":
+                            // Lógica de ordenación para "Nombre Descendente"
+                            $orderBy = " ORDER BY BID_nom DESC";
+                            break;
+                        default:
+                            // Opción de orden desconocida
+                            $orderBy = ""; // Si no hay una opción válida, la cadena de ordenación es vacía
+                            break;
+                    }
+                
+                    $sql .= $orderBy;
+                }
+                $stmt = $conn->prepare($sql);
+                
+                // Concatenar '%' al término de búsqueda para buscar coincidencias parciales
+                $searchTerm = "%" . $searchTerm . "%";
+
+                $stmt->bind_param("sss", $searchTerm, $searchTerm, $searchTerm);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                
+                // Mostrar resultados de la búsqueda
+                if ($result->num_rows > 0) {
+                    
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<div class='contenedores'>";
+                        echo "<p>";
+                        echo "<img src='images/" . $row['BID_imagen_url'] . "' alt='Imagen del bidón'>";
+
+                        echo "<p><h4>" . $row['BID_nom'] . "</h4><br>";
+                        echo "Precio: $" . $row['BID_precio'] . "<br>";
+                        echo "Litros: " . $row['BID_litros'] . "<br>";
+                        echo "Stock: " . $row['BID_stock'] . "</p>";
+                        echo "</p>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "<p>No se encontraron resultados.</p>";
+                }
+                $conn->close();
+            ?>
+        </div>
+    </div>
     <footer>
 
     </footer>
